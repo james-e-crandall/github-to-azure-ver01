d4969892778a17776a39c3988c8c630f6db9d68d
james-e-crandall <james.e.crandall@gmail.com>
2026-06-29 15:34:21 -0500

    (#4)

--- github-to-azure.AppHost/AppHost.cs
+++ github-to-azure.AppHost/AppHost.cs
@@ -1,3 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var configapi = builder.AddProject<Projects.ConfigApi>("configapi");
+
+var gateway = builder.AddYarp("gateway")
+    .WithConfiguration(yarp =>
+    {
+        yarp.AddRoute(configapi);
+        yarp.AddRoute("/api/{**catch-all}", configapi);
+    });
+
+
 builder.Build().Run();
